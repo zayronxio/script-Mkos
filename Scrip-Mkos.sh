7789cbf8226a3bfe388179552ef8d5737856da80
zayronxio <zayronxio@gmail.com>
2021-07-25 11:23:03 -0500

    Picom deb v1

---
+++ Scrip-Mkos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+##[es] Script para cambiar apariencia de xubuntu 21.04. 
+##[en] Script for change the xubuntu 21.04 appearance 
+#Creator: Adolfo Silerio a.k.a ZAYRONXIO
+#mail: zayronxio@gmail.com
+###########
+
+
+#[en]Install repositories and apps
+#[es]Instalando repocitorios y apps:
